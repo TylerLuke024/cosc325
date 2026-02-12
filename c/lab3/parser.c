6a207c7bde47fd6f1630fc002fb117257ec78b08
Tyler <tylerluke024@gmail.com>
2026-02-12 08:21:01 -0600

    Stage All Commit

--- c/lab3/parser.c
+++ c/lab3/parser.c
@@ -1,132 +1,180 @@
-#include "lexer.c" // never supposed to add a c file into another c file (bad practice)
+#include "lexer.c"
 
 // data structures needed for the interpreter
-int lineno = 0; // if this is equal to 0 htne we should execute immediatly
-char* lines[10]; // preallocate enough room for 10 lines
-int linenos[10]; 
+int lineno = 0;   // if this is equal to 0 then we should execute immediately
+char* lines[10];  // preallocate enough room for 10 lines
+int linenos[10];  
 
 void line();
 void statement();
 void expr_list();
 void expression();
 void relop();
+void term();
 
 /******************************************************/
 /* main driver */
-int main() {
-   /* Open the input data file and process its contents */
-   if  ((in_fp = fopen("front.in", "r")) == NULL)
-     printf("ERROR - cannot open front.in \n");
-   else  {
-     getChar();
-     do   {
-       lex();
-       line();
-    }  while (nextToken != EOF);
+int main()
+{
+  /* Open the input data file and process its contents */
+  if ((in_fp = fopen("front.in", "r")) == NULL)
+    printf("ERROR - cannot open front.in \n");
+  else
+  {
+    getChar();
+    do
+    {
+      lex();
+      line();
+    } while (nextToken != EOF);
   }
 }
 
 // lex() MUST be called before this function
-void line(){
-    if(nextToken == NUMBER) {
+void line() {
+    if (nextToken == NUMBER) {
         lineno = atoi(lexeme);
-        // take whatever is left in the rest of the oline and store it and process it later
+        // take whatever is left in the rest of the line and store it for processing later!
 
-        // consume the token by looking at the line number and storing the statement that follows in the right place
+        // consume the token by looking at the line number
+        // and storing the statement that follows in the right place in our stored program
         // BUT NOT FOR THIS ASSIGNMENT
         // Call lex() to get the next token
         lex();
-    } 
-    statement(); // note that statement MUST have an extra call to lex()
-    if (nextToken != CR) {
-        printf("Expecting CR, but found: %d instead!\n", nextToken);
     }
+    statement(); // note that statement MUST have an extra call to lex()
+    if (nextToken != CR && nextToken != EOF) {
+        printf("Expecting CR, but found: %d instead!\n", nextToken);
+    } 
+
 }
 
-// lex() must have already been called before here
-void statement() { // keep going with more cases INPUT DOES NOT NEED THE EXTRA CALL TO LEX (RETURN and below dont need )
-    switch(nextToken){
+// lex() MUST have already been called before here
+void statement() {
+    switch(nextToken) {
         case PRINT:
             lex();
-            expre_list();
+            expr_list();
+            // unconditionally printf("\n");
             break;
         case IF:
             lex();
-            expression();
+            expression(); 
             lex();
             relop();
             lex();
             expression();
+            lex();
+            if (nextToken != THEN) {
+                printf("error! expecting then found something else");
+            }
+            lex();
             statement();
-            // we never need an extra call to lex() here
+            // we never need an extra call to lex() here 
             // because statement() ALWAYS has an extra call to lex()
             break;
         case GOTO:
             lex();
 
-            //extra call to lex to look for the character return
+            // extra call to lex to look for the character return
             lex();
             break;
+
+        // keep going with more cases INPUT DOES NOT NEED THE EXTRA CALL TO LEX ... NEITHER DO THE ONES THAT ARE JUST KEYWORDS
         case INPUT:
             lex();
 
             break;
+
         case LET:
             lex();
 
-            //extra call to lex to look for the character return
+            // extra call to lex to look for the carriage return
             lex();
             break;
+        
         case GOSUB:
             lex();
+            expression();
 
-            //extra call to lex to look for the character return
+            // extra call to lex to look for the carriage return
             lex();
             break;
-
+            
         case RETURN:
         case CLEAR:
         case LIST:
         case RUN:
         case END:
-            lex();
+            lex(); // this IS the extra call to lex() since nothing comes after these keywords
+            // you probably need another call to lex() right here!!!!
             break;
-        
     }
 }
 
 // makes an extra call to lex() to look for the comma
-// lex has already been called before expr_list
+// lex has ALREADY been called before expr_list
 void expr_list() {
     if (nextToken == STRING) {
-        if (lineno > 0) {
-            printf("%s\n",lexeme);
-        } else {
-            expression();
-        }
+        // do nothing for this assignment
+        // but in the next assignment you will need to print something!
+    } else {
+        expression();
     }
     lex(); // extra call to look for the comma
     while (nextToken == COMMA) {
+        // next assignment: printf("\t");
         if (nextToken == STRING) {
-            if (lineno > 0) {
-                printf("%s\n",lexeme);
-            } else {
-                expression();
-            }
+            // do nothing for this assignment
+            // but in the next assignment you will need to print something
+        } else {
+            expression();
         }
         lex(); // extra call to look for the comma
-        // there are only two valud tokens AT THIS SPOT
-        if (nextToken != COMMA && nextToken != CR){
+        // there are only two valid tokens AT THIS SPOT
+        if (nextToken != COMMA && nextToken != CR) {
             printf("Expecting COMMA or CR but found: %d\n", nextToken);
-            exit(-1);
+            exit(1);
         }
     }
 }
 
-void expression(){
+void expression() {
+    lex();
+    if (nextToken == ADD_OP) {
+        // do nothing for this assignment
+        // but in the next assignment you will need to print something!
+    } else if (nextToken == SUB_OP) {
+        // do nothing for this assignment
+        // but in the next assignment you will need to print something!
+    } else {
+        // do nothing for this assignment
+        // but in the next assignment you will need to print something!
+    }
+    term();
+    lex();
+    while (nextToken == ADD_OP || nextToken == SUB_OP || nextToken == SUB_OP) {
+        if (nextToken == ADD_OP) {
+        // do nothing for this assignment
+        // but in the next assignment you will need to print something!
+        } else {
+            // do nothing for this assignment
+            // but in the next assignment you will need to print something!
+        }
+        term();
+        lex();
+        if (!(nextToken == ADD_OP || nextToken == SUB_OP || nextToken == SUB_OP || nextToken == CR)) {
+            printf("Expecting ADD_OP or SUB_OPP or CR but found: %d\n", nextToken);
+            exit(1);
+        }
+
+    }
+}
+
+void relop() {
 
 }
 
-void relop(){
+void term() {
 
 }
---
+++ c/lab3/pointers.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    char* name = "Tyler Luke";
+    printf("The memory address of name is: %p\n", name);
+    printf("The value stored in name is: %s", name);
+    return 0;
+}
